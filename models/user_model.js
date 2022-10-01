09b8c5a644324d3adcc245ee4aa6f341590c1caa
Your Name <you@example.com>
2022-10-01 22:17:56 +0530

    first commit

---
+++ models/user_model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'USER'
+    }
+});
+
+mongoose.model("UserModel", userSchema);
